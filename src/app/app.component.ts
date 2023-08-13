c5587cc707e7ae21596bbe3c19df96e8a29fad17
sayedhany <sayedh@mail.ru>
2023-08-13 12:53:47 +0300

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'personal-dashvoard';
+  time: string;
+  date: string;
+  ngOnInit() {
+    this.time = new Date().toLocaleTimeString('en-US', {
+      hour12: false,
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+    this.date = new Date().toLocaleString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
+    console.log(this.date);
+  }
 }
